1368223b392d06dd07864440d6a8be90d3aab6e3
djballowe <djballowe@gmail.com>
2022-09-19 16:30:14 -0700

    Herkoku Deploy

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,9 @@ mongoose.connection.on("connected", () => {
   console.log("connected");
 });
 
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-    });
-  }
+  app.use(express.static("build"));
+}
 
 app.use(express.json());
 app.use(cors());
